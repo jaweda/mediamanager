481c7037d4715b32f0d5e7671f39538c9a7f8c64
Jamie Furness <jfurness@yammer-inc.com>
2013-02-25 23:02:04 +0000

    Attempting to fix intermittent downloading of invalid torrent files

--- src/main/java/com/jamierf/mediamanager/downloader/WatchDirDownloader.java
+++ src/main/java/com/jamierf/mediamanager/downloader/WatchDirDownloader.java
@@ -1,6 +1,7 @@
 package com.jamierf.mediamanager.downloader;
 
 import com.google.common.hash.Hashing;
+import com.google.common.io.ByteStreams;
 import com.google.common.io.Files;
 import com.jamierf.mediamanager.io.retry.RetryManager;
 import com.yammer.dropwizard.client.JerseyClient;
@@ -9,10 +10,7 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.URI;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
@@ -20,22 +18,21 @@ import java.util.concurrent.Executors;
 
 public class WatchDirDownloader implements Downloader {
 
+    private static String getFilename(URI link) {
+        final String value = link.toString();
+        final String hash = Hashing.sha1().hashString(value).toString();
+
+        return String.format("%s.torrent", hash);
+    }
+
     private final JerseyClient client;
     private final RetryManager retryManager;
     private final File watchDir;
-    private final File tempDir;
-    private final ExecutorService workerPool;
 
 	public WatchDirDownloader(JerseyClient client, RetryManager retryManager, File watchDir) {
         this.client = client;
         this.retryManager = retryManager;
         this.watchDir = watchDir;
-
-        // Create a temp dir for storing the torrent files until they are ready to be moved
-        tempDir = Files.createTempDir();
-        tempDir.deleteOnExit();
-
-        workerPool = Executors.newCachedThreadPool();
 
         if (!watchDir.exists())
             watchDir.mkdirs();
@@ -62,28 +59,14 @@ public class WatchDirDownloader implements Downloader {
 
 	@Override
 	public void download(final URI link) throws IOException {
-        final String filename = String.format("%s.torrent", Hashing.sha1().hashString(link.toString()).toString());
-
-        final File tempFile = new File(tempDir, filename);
+        final String filename = WatchDirDownloader.getFilename(link);
         final File watchedFile = new File(watchDir, filename);
 
-        workerPool.submit(new Callable<Object>() {
-            @Override
-            public Object call() throws IOException {
-                final InputStream in = openStream(link);
-                final FileOutputStream out = new FileOutputStream(tempFile);
-
-                try {
-                    IOUtils.copy(in, out);
-                }
-                finally {
-                    in.close();
-                    out.close();
-                }
-
-                Files.move(tempFile, watchedFile);
-                return null;
-            }
-        });
+        try (
+                final BufferedInputStream in = new BufferedInputStream(this.openStream(link));
+                final FileOutputStream out = new FileOutputStream(watchedFile);
+        ) {
+            IOUtils.copy(in, out);
+        }
 	}
 }
